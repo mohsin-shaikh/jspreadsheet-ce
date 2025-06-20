e80dfe1a808d0e78c41b63fb85a4d46cbf1d0d15
Mohsin Shaikh <mohsin.shaikh@eappsys.com>
2025-06-20 21:12:42 +0530

    Feat(tabs): update toolbar and worksheet dimensions
    - Disabled the toolbar for the spreadsheet instance.
    - Increased minimum dimensions of the worksheet to [10, 10].
    - Added a dropdown column for gender selection.
    
    Don't forget to commit! git commit -m "Feat(tabs): update toolbar and worksheet dimensions"

--- src/test.js
+++ src/test.js
@@ -7,9 +7,16 @@ window.jss = jspreadsheet;
 
 window.instance = jspreadsheet(root, {
     tabs: true,
-    toolbar: true,
+    toolbar: false,
     worksheets: [{
-        minDimensions: [6,6],
+        minDimensions: [10, 10],
+        columns: [
+            { type: 'dropdown', source: ['Male', 'Female'] }
+        ],
+        // Not working
+        // cells: {
+        //     A1: { type: 'dropdown', source: ['Male', 'Female'] }
+        // },
     }],
 })
 
